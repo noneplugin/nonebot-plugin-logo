f035e5d5decbb42985f3d44ecca1f7e038410a32
meetwq <meetwq@gmail.com>
2022-06-20 23:32:24 +0800

    适配插件元数据

--- nonebot_plugin_logo/__init__.py
+++ nonebot_plugin_logo/__init__.py
@@ -1,58 +1,66 @@
 import shlex
+import traceback
 from nonebot import on_command
+from nonebot.log import logger
 from nonebot.matcher import Matcher
 from nonebot.typing import T_Handler
 from nonebot.params import CommandArg
+from nonebot.plugin import PluginMetadata
 from nonebot.adapters.onebot.v11 import MessageSegment, Message
 
-from .data_source import create_logo, commands
+from .data_source import commands, Command
 
-__help__plugin_name__ = 'logo'
-__des__ = 'pornhub等风格logo生成'
-__cmd__ = '''
-pornhub：ph {text1} {text2}
-youtube：yt {text1} {text2}
-5000兆円欲しい!：5000兆 {text1} {text2}
-抖音：douyin {text}
-谷歌：google {text}
-'''.strip()
-__short_cmd__ = 'ph、yt、5000兆、douyin'
-__example__ = '''
-ph Porn Hub
-yt You Tube
-5000兆 我去 初音未来
-douyin douyin
-'''.strip()
-__usage__ = f'{__des__}\nUsage:\n{__cmd__}\nExample:\n{__example__}'
-
-
-async def handle(matcher: Matcher, style: str, text: str):
-    arg_num = commands[style]['arg_num']
-    texts = [text] if arg_num == 1 else shlex.split(text)
-    if len(texts) != arg_num:
-        await matcher.finish('参数数量不符')
-
-    image = await create_logo(style, texts)
-    if image:
-        await matcher.finish(MessageSegment.image(image))
-    else:
-        await matcher.finish('出错了，请稍后再试')
+__plugin_meta__ = PluginMetadata(
+    name="logo生成",
+    description="pornhub等风格logo生成",
+    usage=(
+        "pornhub：ph {text1} {text2}"
+        "youtube：yt {text1} {text2}"
+        "5000兆円欲しい!：5000兆 {text1} {text2}"
+        "抖音：douyin {text}"
+        "谷歌：google {text}"
+    ),
+    extra={
+        "unique_name": "logo",
+        "example": "ph Porn Hub\nyt You Tube\n5000兆 我去 初音未来",
+        "author": "meetwq <meetwq@gmail.com>",
+        "version": "0.2.2",
+    },
+)
 
 
 def create_matchers():
-    def create_handler(style: str) -> T_Handler:
-        async def handler(msg: Message = CommandArg()):
+    def create_handler(command: Command) -> T_Handler:
+        async def handler(matcher: Matcher, msg: Message = CommandArg()):
             text = msg.extract_plain_text().strip()
             if not text:
                 await matcher.finish()
-            await handle(matcher, style, text)
+
+            arg_num = command.arg_num
+            if arg_num == 1:
+                texts = [text]
+            else:
+                try:
+                    texts = shlex.split(text)
+                except:
+                    texts = text.split()
+            if len(texts) != arg_num:
+                await matcher.finish(f"参数数量不符，需要发送{arg_num}段文字")
+
+            try:
+                image = await command.func(texts)
+            except:
+                logger.warning(traceback.format_exc())
+                await matcher.finish("出错了，请稍后再试")
+
+            await matcher.finish(MessageSegment.image(image))
 
         return handler
 
-    for style, params in commands.items():
-        matcher = on_command(style, aliases=params['aliases'],
-                             priority=13, block=True)
-        matcher.append_handler(create_handler(style))
+    for command in commands:
+        on_command(
+            command.keywords[0], aliases=set(command.keywords), priority=13, block=True
+        ).append_handler(create_handler(command))
 
 
 create_matchers()
--- nonebot_plugin_logo/data_source.py
+++ nonebot_plugin_logo/data_source.py
@@ -1,109 +1,99 @@
 import base64
 import jinja2
 import imageio
-import traceback
 from io import BytesIO
 from pathlib import Path
-from typing import List, Union
+from dataclasses import dataclass
+from typing import List, Tuple, Union, Protocol
 
-from nonebot.log import logger
+from nonebot import require
+
+require("nonebot_plugin_htmlrender")
 from nonebot_plugin_htmlrender import get_new_page, html_to_pic
 
 
 dir_path = Path(__file__).parent
-template_path = dir_path / 'templates'
+template_path = dir_path / "templates"
 path_url = f"file://{template_path.absolute()}"
-env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_path),
-                         enable_async=True)
+env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(template_path), enable_async=True
+)
 
 
-async def create_image(html: str):
-    return await html_to_pic(html, viewport={"width": 100, "height": 100},
-                             template_path=path_url)
+async def to_image(html: str) -> bytes:
+    return await html_to_pic(
+        html, viewport={"width": 100, "height": 100}, template_path=path_url
+    )
 
 
-async def create_pornhub_logo(left_text, right_text) -> bytes:
-    template = env.get_template('pornhub.html')
-    html = await template.render_async(left_text=left_text, right_text=right_text)
-    return await create_image(html)
+async def make_pornhub(texts: List[str]) -> bytes:
+    template = env.get_template("pornhub.html")
+    html = await template.render_async(left_text=texts[0], right_text=texts[1])
+    return await to_image(html)
 
 
-async def create_youtube_logo(left_text, right_text) -> bytes:
-    template = env.get_template('youtube.html')
-    html = await template.render_async(left_text=left_text, right_text=right_text)
-    return await create_image(html)
+async def make_youtube(texts: List[str]) -> bytes:
+    template = env.get_template("youtube.html")
+    html = await template.render_async(left_text=texts[0], right_text=texts[1])
+    return await to_image(html)
 
 
-async def create_5000choyen_logo(top_text, bottom_text) -> str:
-    template = env.get_template('5000choyen.html')
-    html = await template.render_async(top_text=top_text, bottom_text=bottom_text)
-
-    async with get_new_page() as page:
-        await page.goto(path_url)
-        await page.set_content(html)
-        await page.wait_for_selector('a')
-        a = await page.query_selector('a')
-        img = await (await a.get_property('href')).json_value()
-    return 'base64://' + str(img).replace('data:image/png;base64,', '')
-
-
-async def create_douyin_logo(text) -> BytesIO:
-    template = env.get_template('douyin.html')
-    html = await template.render_async(text=text, frame_num=10)
+async def make_5000choyen(texts: List[str]) -> str:
+    template = env.get_template("5000choyen.html")
+    html = await template.render_async(top_text=texts[0], bottom_text=texts[1])
 
     async with get_new_page() as page:
         await page.goto(path_url)
         await page.set_content(html)
-        imgs = await page.query_selector_all('a')
-        imgs = [await (await img.get_property('href')).json_value() for img in imgs]
+        await page.wait_for_selector("a")
+        a = await page.query_selector("a")
+        assert a
+        img = await (await a.get_property("href")).json_value()
+    return "base64://" + str(img).replace("data:image/png;base64,", "")
 
-    imgs = [imageio.imread(base64.b64decode(
-        str(img).replace('data:image/png;base64,', ''))) for img in imgs]
+
+async def make_douyin(texts: List[str]) -> BytesIO:
+    template = env.get_template("douyin.html")
+    html = await template.render_async(text=texts[0], frame_num=10)
+
+    async with get_new_page() as page:
+        await page.goto(path_url)
+        await page.set_content(html)
+        imgs = await page.query_selector_all("a")
+        imgs = [await (await img.get_property("href")).json_value() for img in imgs]
+
+    imgs = [
+        imageio.imread(base64.b64decode(str(img).replace("data:image/png;base64,", "")))
+        for img in imgs
+    ]
 
     output = BytesIO()
-    imageio.mimsave(output, imgs, format='gif', duration=0.2)
+    imageio.mimsave(output, imgs, format="gif", duration=0.2)
     return output
 
 
-async def create_google_logo(text) -> BytesIO:
-    template = env.get_template('google.html')
-    html = await template.render_async(text=text)
-    return await create_image(html)
+async def make_google(texts: List[str]) -> bytes:
+    template = env.get_template("google.html")
+    html = await template.render_async(text=texts[0])
+    return await to_image(html)
 
 
-commands = {
-    'pornhub': {
-        'aliases': {'ph ', 'phlogo'},
-        'func': create_pornhub_logo,
-        'arg_num': 2
-    },
-    'youtube': {
-        'aliases': {'yt ', 'ytlogo'},
-        'func': create_youtube_logo,
-        'arg_num': 2
-    },
-    '5000choyen': {
-        'aliases': {'5000兆', '5000choyen'},
-        'func': create_5000choyen_logo,
-        'arg_num': 2
-    },
-    'douyin': {
-        'aliases': {'dylogo'},
-        'func': create_douyin_logo,
-        'arg_num': 1
-    },
-    'google': {
-        'aliases': {'gglogo'},
-        'func': create_google_logo,
-        'arg_num': 1
-    }
-}
+class Func(Protocol):
+    async def __call__(self, texts: List[str]) -> Union[str, bytes, BytesIO, Path]:
+        ...
 
 
-async def create_logo(style: str, texts: List[str]) -> Union[str, bytes, BytesIO, Path]:
-    try:
-        func = commands[style]['func']
-        return await func(*texts)
-    except:
-        logger.warning(traceback.format_exc(limit=1))
-        return None
+@dataclass
+class Command:
+    keywords: Tuple[str, ...]
+    func: Func
+    arg_num: int = 1
+
+
+commands = [
+    Command(("pornhub", "ph ", "phlogo"), make_pornhub, 2),
+    Command(("youtube", "yt ", "ytlogo"), make_youtube, 2),
+    Command(("5000choyen", "5000兆"), make_5000choyen, 2),
+    Command(("douyin", "dylogo"), make_douyin),
+    Command(("google", "gglogo"), make_google),
+]
